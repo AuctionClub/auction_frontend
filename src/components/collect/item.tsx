ec689fa627a0460e7376d9a17a3c40e9f3ee9295
feng <1416929275@qq.com>
2024-05-17 08:33:38 +0800

    feat：获取账户下nft

--- src/components/collect/item.tsx
+++ src/components/collect/item.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
-import { LapTimerIcon } from '@radix-ui/react-icons'
-
+import bg_default from '/public/bg_default.png'
 type CollectItemProps = {
   item: any
 }
@@ -9,14 +8,15 @@ export default function CollectItem({ item }: CollectItemProps) {
   return (
     <div className="w-[250px] rounded-lg shadow-lg overflow-hidden mr-5 last:mr-0 cursor-pointer relative top-0 hover:-top-2 transition-[top] duration-100 ease-in">
       <Image
-        src={item.img}
+        src={item.logoUrl || bg_default}
         width={250}
         height={250}
         alt="Picture of the author"
       />
       <div className="px-2 py-4 relative">
-        <p className="font-bold text-lg mb-1">NFT Name</p>
-        <div className="text-sm flex justify-between">
+        <p className="font-bold text-sm">{item.tokenId}</p>
+        <p className="font-bold text-lg mb-1">{item.token}</p>
+        {/* <div className="text-sm flex justify-between">
           <div className="flex flex-col">
             <p>Minting</p>
             <p className="font-bold">{item.mintDate}</p>
@@ -25,7 +25,7 @@ export default function CollectItem({ item }: CollectItemProps) {
             <p>Price</p>
             <p className="font-bold">{item.price}</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- src/components/collect/list.tsx
+++ src/components/collect/list.tsx
@@ -1,73 +1,69 @@
+'use client'
 import { ScrollArea } from '@radix-ui/themes'
 import { ChevronRightIcon } from '@radix-ui/react-icons'
 import AuctionItem from '@/components/collect/item'
+import { OKLINK_KEY } from '@/constants'
+import {
+  useAccount,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+  useBalance,
+  useReadContract,
+} from 'wagmi'
+import { useEffect, useState } from 'react'
 
 export default function CollectList() {
-  const list = [
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '1.23ETH',
-      tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
-      currentBid: '10ETH',
-      currentBidder: '0x111112222',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '1.23ETH',
-      tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
-      currentBid: '10ETH',
-      currentBidder: '0x8888888111122222233444444',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '1.23ETH',
-      tags: ['英式拍卖', 'pleasure', '狂热'],
-      currentBid: '10ETH',
-      currentBidder: '0x111112222',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '10ETH',
-      tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
-      currentBid: '10ETH',
-      currentBidder: '0x111112222',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '0.5ETH',
-      tags: ['荷兰拍卖', '狂热', '慈善'],
-      currentBid: '10ETH',
-      currentBidder: '0x111112222',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '6ETH',
-      tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
-      currentBid: '10ETH',
-      currentBidder: '0x111112222',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-    {
-      img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
-      price: '1.23ETH',
-      tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
-      currentBid: '10ETH',
-      currentBidder: '0x111112222',
-      deadline: '2024-01-01 00:00:00',
-      mintDate: '2024-05-03',
-    },
-  ]
+  const account = useAccount()
+  const [list, setList] = useState([])
+  // const list = [
+  //   {
+  //     img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
+  //     price: '1.23ETH',
+  //     tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
+  //     currentBid: '10ETH',
+  //     currentBidder: '0x111112222',
+  //     deadline: '2024-01-01 00:00:00',
+  //     mintDate: '2024-05-03',
+  //   },
+  //   {
+  //     img: 'https://i.seadn.io/s/raw/files/b8c7c610c8a6ffbc66db01a6c6559ebc.png?auto=format&dpr=1&w=1000',
+  //     price: '1.23ETH',
+  //     tags: ['荷兰拍卖', 'pleasure', '狂热', '慈善'],
+  //     currentBid: '10ETH',
+  //     currentBidder: '0x8888888111122222233444444',
+  //     deadline: '2024-01-01 00:00:00',
+  //     mintDate: '2024-05-03',
+  //   },
+  // ]
+  async function getData() {
+    const res = await fetch(
+      `https://www.oklink.com/api/v5/explorer/nft/address-balance-fills?chainShortName=SEPOLIA_TESTNET&address=${account.address}&protocolType=token_721&limit=100`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Ok-Access-Key': OKLINK_KEY as string,
+        },
+      }
+    )
+
+    if (!res.ok) {
+      // 由最近的 error.js 处理
+      throw new Error('Failed to fetch data')
+    }
+    const { data } = await res.json()
+
+    if (data.length) {
+      const _list = data[0].tokenList
+      setList(_list)
+    }
+  }
+  useEffect(() => {
+    if (account.address) {
+      getData()
+    }
+  }, [account])
+  if (!list.length) return null
   return (
     <div className="my-20 flex place-content-center relative">
       <div className="md:max-w-screen-lg flex-1 2xl:max-w-7xl px-6 md:px-10">
@@ -85,8 +81,8 @@ export default function CollectList() {
           scrollbars="horizontal"
         >
           <div className="flex justify-between mb-5 mt-5">
-            {list.map((i) => {
-              return <AuctionItem item={i} />
+            {list.map((i, index) => {
+              return <AuctionItem item={i} key={index} />
             })}
           </div>
         </ScrollArea>
