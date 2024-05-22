4ab8ae9b60862e78582454aed55f054334cb4fa5
尹雪剑 <gony952701@gmail.com>
2024-05-22 16:55:15 +0800

    feat: type

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import * as Tooltip from '@radix-ui/react-tooltip';
+import React from "react";
+import * as Tooltip from "@radix-ui/react-tooltip";
 // 给...rest定义类型 是为了传递给Tooltip.Content的属性 例如align
-const TooltipProvider = ({text,children,...rest}:
-    {text:string,children:React.ReactNode} & React.ComponentProps<typeof Tooltip.Content>
-) => {
+const TooltipProvider = ({
+  text,
+  children,
+  ...rest
+}: { text: string; children: React.ReactNode } & React.ComponentProps<
+  typeof Tooltip.Content
+>) => {
   return (
     <Tooltip.Provider>
       <Tooltip.Root>
@@ -14,7 +18,7 @@ const TooltipProvider = ({text,children,...rest}:
         </Tooltip.Trigger>
         <Tooltip.Portal>
           <Tooltip.Content
-          {...rest}
+            {...rest}
             className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
             sideOffset={5}
           >
@@ -25,6 +29,6 @@ const TooltipProvider = ({text,children,...rest}:
       </Tooltip.Root>
     </Tooltip.Provider>
   );
-}
+};
 
-export default TooltipProvider
+export default TooltipProvider;
