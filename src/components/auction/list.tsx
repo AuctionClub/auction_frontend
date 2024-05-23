63aeed4cd17548104aa3a0fbbd00921c0e905cac
尹雪剑 <gony952701@gmail.com>
2024-05-23 19:15:24 +0800

    feat: listdata add tokenId&contractAddress

--- src/components/auction/list.tsx
+++ src/components/auction/list.tsx
@@ -6,6 +6,8 @@ import PopoverWarp from "../popover";
 export default function AuctionList() {
   const list = [
     {
+      tokenId: 1,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "1.23ETH",
       tags: ["荷兰拍卖", "pleasure", "狂热", "慈善"],
@@ -14,6 +16,8 @@ export default function AuctionList() {
       deadline: "2024-01-01 00:00:00",
     },
     {
+      tokenId: 2,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "1.23ETH",
       tags: ["荷兰拍卖", "pleasure", "狂热", "慈善"],
@@ -22,6 +26,8 @@ export default function AuctionList() {
       deadline: "2024-01-01 00:00:00",
     },
     {
+      tokenId: 3,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "1.23ETH",
       tags: ["英式拍卖", "pleasure", "狂热"],
@@ -30,6 +36,8 @@ export default function AuctionList() {
       deadline: "2024-01-01 00:00:00",
     },
     {
+      tokenId: 7,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "10ETH",
       tags: ["荷兰拍卖", "pleasure", "狂热", "慈善"],
@@ -38,6 +46,8 @@ export default function AuctionList() {
       deadline: "2024-01-01 00:00:00",
     },
     {
+      tokenId: 4,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "0.5ETH",
       tags: ["荷兰拍卖", "狂热", "慈善"],
@@ -46,6 +56,8 @@ export default function AuctionList() {
       deadline: "2024-01-01 00:00:00",
     },
     {
+      tokenId: 5,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "6ETH",
       tags: ["荷兰拍卖", "pleasure", "狂热", "慈善"],
@@ -54,6 +66,8 @@ export default function AuctionList() {
       deadline: "2024-01-01 00:00:00",
     },
     {
+      tokenId: 6,
+      contractAddress: "0xxxx",
       img: "https://i.seadn.io/s/raw/files/0bcb42f07224f4b66642aa7771d9a828.png?auto=format&dpr=1&w=1000",
       price: "1.23ETH",
       tags: ["荷兰拍卖", "pleasure", "狂热", "慈善"],
