e5df313bcb6edbd6fc7516d99a505c2251ff617f
feng <1416929275@qq.com>
2024-05-25 11:45:49 +0800

    feat：add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { cn } from "@/lib/utils";
-
 import HeroSection from "@/components/hero-section";
 import AuctionList from "@/components/auction/list";
-import CollectList from "@/components/collect/list";
+import Footer from "@/components/footer";
 
 export default function Page() {
   return (
-    <div className="pb-96">
+    <div>
       <HeroSection />
       <AuctionList />
-      <CollectList />
+      <Footer />
     </div>
   );
 }
