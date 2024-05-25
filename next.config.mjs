0bfd72bf2eb23483d53b1f6f455adab418a976ed
Yxj <gony952701@gmail.com>
2024-05-25 11:25:04 +0800

    chore: workflow test

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
   assetPrefix: isProd ? "https://diyauction.github.io/auction_frontend/" : "",
   images: {
     remotePatterns: [
